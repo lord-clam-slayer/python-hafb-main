cc6f7233c89fdca982822662800793163bf05950
lord-clam-slayer <thedude8910@gmail.com>
2021-08-09 09:45:33 -0600

    initial setup for template

--- hello.py
+++ hello.py
@@ -1,8 +1,21 @@
 print("Hello Waldo")
-
+"""
+Author: me
+"""
 import argparse
 
-parser = argparse.ArgumentParser(description ='Say Hello')
-parser.add_argument('name', help = 'Name to greet')
-args = parser.parse_args()
-print('Hello, ' + args.name + '!')
+def get_args():
+    parser = argparse.ArgumentParser(description='Say Hello')
+    parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
+    return parser.parse_args()
+def main():
+    """
+    This is where the action is
+    :return: None
+    """
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+if __name__ == '__main__':
+    main()
+print(__name__)
